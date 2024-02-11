2d3930b68ee4f1fc89e6e36f00b77c5317732813
anton <schliesst06.erweitern@icloud.com>
2024-02-11 12:35:00 +0100

    icons aktualisiert

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Icons used:</p>
+    <a href="https://www.flaticon.com/free-icons/euro" title="euro icons">Euro icons created by juicy_fish - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/report" title="report icons">Report icons created by Freepik - Flaticon</a>
   </div>
 </template>
 
