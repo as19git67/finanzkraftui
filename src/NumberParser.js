12250146f6a65d8ac72b470ab3834823d6ccba68
Anton Schegg <aschegg@opentext.com>
2024-04-02 13:34:31 +0200

    Number parser fixed

--- src/NumberParser.js
+++ src/NumberParser.js
@@ -1,24 +1,30 @@
 export default class NumberParser {
   constructor(locale) {
     const parts = new Intl.NumberFormat(locale).formatToParts(12345.6);
-    const numerals = [...new Intl.NumberFormat(locale, {useGrouping: false}).format(9876543210)].reverse();
+    // eslint-disable-next-line max-len
+    const numerals = [...new Intl.NumberFormat(locale, { useGrouping: false }).format(9876543210)].reverse();
     const index = new Map(numerals.map((d, i) => [d, i]));
-    this._group = new RegExp(`[${parts.find(d => d.type === "group").value}]`, "g");
-    this._decimal = new RegExp(`[${parts.find(d => d.type === "decimal").value}]`);
-    this._numeral = new RegExp(`[${numerals.join("")}]`, "g");
-    this._index = d => index.get(d);
+    this._group = new RegExp(`[${parts.find((d) => d.type === 'group').value}]`, 'g');
+    this._decimal = new RegExp(`[${parts.find((d) => d.type === 'decimal').value}]`);
+    this._numeral = new RegExp(`[${numerals.join('')}]`, 'g');
+    this._index = (d) => index.get(d);
   }
 
-  parse(string) {
-    if (string == null) {
+  parse(value) {
+    if (value == null) {
       return NaN;
     }
-    if (!Number.isNaN(string)) {
-      return string;
+    if (NumberParser.isNumber(value)) {
+      return value; // return if already a number
     }
-    return (string = string.trim()
-      .replace(this._group, "")
-      .replace(this._decimal, ".")
-      .replace(this._numeral, this._index)) ? +string : NaN;
+    const trimmedValue = (value.trim()
+      .replace(this._group, '')
+      .replace(this._decimal, '.')
+      .replace(this._numeral, this._index));
+    return parseFloat(trimmedValue);
+  }
+
+  static isNumber(value) {
+    return typeof value === 'number' && Number.isNaN(value) === false;
   }
 }
